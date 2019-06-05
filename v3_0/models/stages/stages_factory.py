bcf1406582432696568d185695e21351492fe58a
Игорь Дьяченко <i.s.djachenko@gmail.com>
2019-06-06 05:20:29 +0700

    Rewritten stages

--- v3_0/models/stages/stages_factory.py
+++ v3_0/models/stages/stages_factory.py
@@ -1,67 +1,29 @@
+from functools import lru_cache
 from pathlib import Path
-from typing import Type, List, Optional
+from typing import List, Optional
 
-from v3_0 import structure
-from v3_0.structure import Structure
-from v3_0.models.stages.develop_stage import DevelopStage
-from v3_0.models.stages.filter_stage import FilterStage
-from v3_0.models.stages.gif_stage import GifStage
-from v3_0.models.stages.ourate_stage import OurateStage
-from v3_0.models.stages.published_stage import PublishedStage
-from v3_0.models.stages.ready_stage import ReadyStage
+from logic.photoset.checks.readiness_check import ReadinessCheck
+from v3_0.logic.factories.check_factory import CheckFactory
+from v3_0.logic.factories.extractor_factory import ExtractorFactory
 from v3_0.models.stages.stage import Stage
-from v3_0.models.world import World
 
 
 class StagesFactory:
-
     @staticmethod
-    def __name_from_class(cls: Type[Stage]) -> str:
-        return cls.__name__.lower().replace("stage", "")
+    @lru_cache()
+    def instance() -> 'StagesFactory':
+        return StagesFactory(
+            checks_factory=CheckFactory.instance(),
+            extractors_factory=ExtractorFactory.instance()
+        )
 
-    @staticmethod
-    def __name_from_structure(struct: Structure) -> str:
-        return struct.path.name.split(".", 1)[1]
-
-    __STAGE_CLASSES = [
-        GifStage,
-        FilterStage,
-        OurateStage,
-        DevelopStage,
-        ReadyStage,
-        PublishedStage,
-    ]
-
-    __STAGE_STRUCTURES = structure.disk_structure["stages"].substructures
-
-    @staticmethod
-    def __map_stages(world: World) -> List[Stage]:
-        classes_by_name = {StagesFactory.__name_from_class(cls): cls for cls in StagesFactory.__STAGE_CLASSES}
-        structures_by_name = {StagesFactory.__name_from_structure(struct): struct for struct in
-                              StagesFactory.__STAGE_STRUCTURES}
-
-        assert set(classes_by_name.keys()) <= set(structures_by_name.keys())
-
-        names = classes_by_name.keys()
-
-        root_path = world.active_disk.root.path
-
-        stages = []
-
-        for name in names:
-            struct = structures_by_name[name]
-            cls = classes_by_name[name]
-
-            stage = cls(root_path / struct.path)
-
-            stages.append(stage)
-
-        return stages
-
-    def __init__(self, world: World) -> None:
+    def __init__(self, checks_factory: CheckFactory, extractors_factory: ExtractorFactory) -> None:
         super().__init__()
 
-        stages = StagesFactory.__map_stages(world)
+        self.__checks_factory = checks_factory
+        self.__extractors_factory = extractors_factory
+
+        stages = self.stages()
 
         self.__stages_by_command = {stage.command: stage for stage in stages}
         self.__stages_by_folders = {stage.folder: stage for stage in stages}
@@ -69,8 +31,104 @@ class StagesFactory:
         self.__commands = [stage.command for stage in stages]
 
     @property
+    @lru_cache()
     def commands(self):
-        return self.__commands
+        return [stage.command for stage in self.stages()]
+
+    @lru_cache()
+    def stages(self) -> List[Stage]:
+        return [
+            self.gif(),
+            self.filter(),
+            self.develop(),
+            self.ourate(),
+            self.ready(),
+            self.published(),
+        ]
+
+    @lru_cache()
+    def gif(self) -> Stage:
+        return Stage(
+            path=Path("stage0.gif"),
+            command="gif",
+            incoming_checks=[],
+            outcoming_checks=[],
+            preparation_hooks=[]
+        )
+
+    @lru_cache()
+    def filter(self) -> Stage:
+        return Stage(
+            path=Path("stage1.filter"),
+            command="filter",
+            incoming_checks=[],
+            outcoming_checks=[],
+            preparation_hooks=[]
+        )
+
+    @lru_cache()
+    def develop(self) -> Stage:
+        return Stage(
+            path=Path("stage2.develop"),
+            command="develop",
+            incoming_checks=[],
+            outcoming_checks=[
+                self.__checks_factory.unselected(),
+                self.__checks_factory.metadata(),
+            ],
+            preparation_hooks=[]
+        )
+
+    @lru_cache()
+    def ourate(self) -> Stage:
+        return Stage(
+            path=Path("stage2.ourate"),
+            command="ourate",
+            incoming_checks=[],
+            outcoming_checks=[
+                self.__checks_factory.unselected(),
+                self.__checks_factory.metadata(),
+            ],
+            preparation_hooks=[
+                self.__extractors_factory.edited()
+            ]
+        )
+
+    @lru_cache()
+    def ready(self) -> Stage:
+        return Stage(
+            path=Path("stage3.ready"),
+            command="ready",
+            incoming_checks=[
+                self.__checks_factory.metadata(),
+                self.__checks_factory.odd_selection(),
+                self.__checks_factory.unselected(),
+                self.__checks_factory.missing_gifs(),
+
+                # todo: investigate and rewrite
+                ReadinessCheck(),
+
+                # todo: no service folders
+                # todo: gifs has been created
+            ],
+            outcoming_checks=[],
+            preparation_hooks=[
+                # todo: instagram
+                # sandbox
+            ]
+        )
+
+    @lru_cache()
+    def published(self) -> Stage:
+        return Stage(
+            path=Path("stage4.published"),
+            command="publish",
+            incoming_checks=[
+                self.__checks_factory.missing_gifs()
+            ],
+            outcoming_checks=[],
+            preparation_hooks=[]
+        )
 
     def stage_by_folder(self, name: str) -> Optional[Stage]:
         return self.__stages_by_folders.get(name)
