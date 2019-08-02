421b0d652d9289cd15705f5721da3b234d57dd08
Igor Djachenko <i.s.djachenko@gmail.com>
2019-08-03 06:18:15 +0700

    Implemented installing project as command line utility

--- v3_0/shared/configuration/configurator.py
+++ v3_0/shared/configuration/configurator.py
@@ -2,20 +2,21 @@ from pathlib import Path
 
 from pyvko.pyvko import Pyvko
 
-from config import Config
 from v3_0.args import Args
+from v3_0.shared.configuration.config import Config
 from v3_0.shared.models.world import World
 from v3_0.shared.singleton import Singleton
 
 
 class Configurator(Singleton):
-    __CONFIG_PATH = "configs/config.json"
+    __CONFIGS_FOLDER = Path(".justin_configs")
+    __CONFIG_FILE = "config.json"
 
     # noinspection PyMethodMayBeStatic
-    def get_args(self) -> Args:
-        config = Config.read(Path(Configurator.__CONFIG_PATH))
+    def get_namespace(self) -> Args:
+        config = Config.read(Path(Configurator.__CONFIGS_FOLDER / Configurator.__CONFIG_FILE))
 
-        pyvko = Pyvko(config.pyvko_config_path)
+        pyvko = Pyvko(Configurator.__CONFIGS_FOLDER / config.pyvko_config)
 
         group = pyvko.get_group(config.group_url)
         photos_uploader = pyvko.get_photos_uploader()
