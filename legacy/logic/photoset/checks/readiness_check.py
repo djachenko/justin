d14513df085f5465cb0a15fc710c7589d3c698d6
Игорь Дьяченко <i.s.djachenko@gmail.com>
2019-08-11 02:27:22 +0700

    Cleaned unused files up

--- legacy/logic/photoset/checks/readiness_check.py
+++
@@ -1,9 +0,0 @@
-from legacy.logic.photoset.selectors.unnecessary_folders_selector import UnnecessaryFoldersSelector
-from v3_0.actions.stage import Check
-
-
-class ReadinessCheck(Check):
-    def __init__(self) -> None:
-        selector = UnnecessaryFoldersSelector(depth=1)
-
-        super().__init__("check of destinations", selector, None)
