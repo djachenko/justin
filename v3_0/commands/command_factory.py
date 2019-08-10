f795beb56931c0709a257af6ee89d8f5199761d8
Igor Djachenko <i.s.djachenko@gmail.com>
2019-08-11 02:34:49 +0700

    Renamed command

--- v3_0/commands/command_factory.py
+++ v3_0/commands/command_factory.py
@@ -3,7 +3,7 @@ from typing import List
 
 from v3_0.commands.command import Command
 from v3_0.commands.delete_posts_command import DeletePostsCommand
-from v3_0.commands.schedule_command import ScheduleCommand
+from v3_0.commands.upload_command import UploadCommand
 from v3_0.commands.stage_command import StageCommand
 from v3_0.actions.stage.models.stages_factory import StagesFactory
 
@@ -18,8 +18,8 @@ class CommandFactory:
     def commands(self) -> List[Command]:
         return [
             self.stage(),
-            self.schedule(),
-            self.delete_posts()
+            self.upload(),
+            self.delete_posts(),
         ]
 
     @lru_cache()
@@ -27,8 +27,8 @@ class CommandFactory:
         return StageCommand(StagesFactory.instance())
 
     @lru_cache()
-    def schedule(self) -> Command:
-        return ScheduleCommand()
+    def upload(self) -> Command:
+        return UploadCommand()
 
     @lru_cache()
     def delete_posts(self) -> Command:
