07fb47d43e3c03db48cde8c6cb892102e82c1ab0
Igor Djachenko <i.s.djachenko@gmail.com>
2019-10-22 17:25:40 +0700

    Rewritten runner according to new dependency injection paradigm

--- v3_0/shared/justin.py
+++ v3_0/shared/justin.py
@@ -1,39 +1,27 @@
 from argparse import Namespace
-from pathlib import Path
 from typing import Callable
 
-from pyvko.pyvko_main import Pyvko
-from pyvko.config.config import Config as PyvkoConfig
+from pyvko.models.group import Group
 
 from v3_0.actions.action import Action
 from v3_0.actions.action_factory import ActionFactory
-from v3_0.shared.configuration.config import Config
-from v3_0.shared.models.world import World
 from v3_0.shared.helpers.singleton import Singleton
+from v3_0.shared.models.world import World
 
 
 class Justin(Singleton):
-    __CONFIGS_FOLDER = Path(".justin_configs")
-    __CONFIG_FILE = "config.json"
 
-    def __init__(self) -> None:
+    def __init__(self, group: Group, world: World, actions_factory: ActionFactory) -> None:
         super().__init__()
 
-        config = Config.read(Justin.__CONFIGS_FOLDER / Justin.__CONFIG_FILE)
+        self.__group = group
+        self.__world = world
 
-        pyvko = Pyvko(PyvkoConfig.read(Justin.__CONFIGS_FOLDER / config.pyvko_config))
-
-        self.__group = pyvko.get_group(config.group_url)
-        self.__world = World.instance()
-
-        self.__actions_factory = ActionFactory.instance()
-
-    def __run_action(self, action: Action, args: Namespace) -> None:
-        action.perform(args, self.__world, self.__group)
+        self.__actions_factory = actions_factory
 
     def __build_action(self, action: Action) -> Callable[[Namespace], None]:
         def inner(args: Namespace) -> None:
-            self.__run_action(action, args)
+            action.perform(args, self.__world, self.__group)
 
         return inner
 
