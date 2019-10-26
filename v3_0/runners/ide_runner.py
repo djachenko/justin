5a8cafdcabea22de25599de7c8681786ffbd7a7b
Igor Djachenko <i.s.djachenko@gmail.com>
2019-10-26 14:53:39 +0700

    Implemented addressing photosets by paths instead of names
    Patterns do work as side effect

--- v3_0/runners/ide_runner.py
+++ v3_0/runners/ide_runner.py
@@ -5,6 +5,6 @@ from v3_0.runners import general_runner
 if __name__ == '__main__':
     general_runner.run(
         Path(__file__).parent.parent.parent,
-        "develop 19.10.23.test_set".split()
+        "develop /Users/justin/photos/stages/stage1.filter/19.10.23.*".split()
         # "upload".split()
     )
