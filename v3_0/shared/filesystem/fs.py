b42dc49352de9dae4bef32e54bb04afc346c89c8
Igor Djachenko <i.s.djachenko@gmail.com>
2019-11-03 07:47:01 +0700

    Probably implemented custom copy func, needs testing

--- v3_0/shared/filesystem/fs.py
+++ v3_0/shared/filesystem/fs.py
@@ -1,3 +1,4 @@
+import platform
 import shutil
 import sys
 from pathlib import Path
@@ -9,6 +10,68 @@ SEPARATOR = "/"
 # noinspection PyUnusedLocal
 def __copy_canceller(src, dst):
     sys.exit("Tried to copy {file_name}, aborting".format(file_name=src))
+
+
+def __get_unix_mount(path: Path) -> Path:
+    while True:
+        if path.is_mount():
+            return path
+
+        path /= ".."
+
+
+def __get_windows_mount(path: Path) -> Path:
+    path = path.absolute()
+
+    return path.parents[-1]
+
+
+def __get_mount(path: Path) -> Path:
+    system_name = platform.system()
+
+    if system_name == "Darwin":
+        return __get_unix_mount(path)
+    elif system_name == "Windows":
+        return __get_windows_mount(path)
+    else:
+        assert False
+
+
+def __flatten(path: Path) -> List[Path]:
+    files = []
+
+    for entry in path.iterdir():
+        if entry.is_file():
+            files.append(entry)
+        elif entry.is_dir():
+            files += __flatten(entry)
+
+    return files
+
+
+def __copy_tree(src_path: Path, dst_path: Path):
+    files = __flatten(src_path)
+
+    total_size = sum(file.stat().st_size for file in files)
+    total_copied = 0
+
+    for index, file in enumerate(files):
+        relative_path = file.relative_to(src_path)
+
+        log_string = f"Copying {relative_path} ({index}/{len(files)}) ({total_copied}/{total_size})"
+
+        print(log_string, end="")
+
+        new_path = dst_path / relative_path
+
+        assert not new_path.exists()
+        assert new_path.parent.is_dir()
+
+        shutil.copy2(file, new_path)
+
+        print("\b" * len(log_string))
+
+    print(f"Finished. Copied {len(files)}/{len(files)} files, {total_size}/{total_size} bytes.")
 
 
 def move(file_path: Path, dest_path: Path):
@@ -34,10 +97,13 @@ def move(file_path: Path, dest_path: Path):
         for item in file_path.iterdir():
             move(item, new_file_path)
 
-        if tree_is_empty(file_path):
+        if __tree_is_empty(file_path):
             remove_tree(file_path)
     else:
-        shutil.move(str(file_path), str(dest_path), __copy_canceller)
+        if __get_mount(file_path) == __get_mount(dest_path):
+            shutil.move(str(file_path), str(dest_path), __copy_canceller)
+        else:
+            __copy_tree(file_path, dest_path)
 
 
 def remove_tree(path: Path) -> None:
@@ -66,5 +132,5 @@ def build_path__(*components):
     return result
 
 
-def tree_is_empty(path: Path):
-    return len(__subfiles(path)) == 0 and all([tree_is_empty(subfolder) for subfolder in __subfolders(path)])
+def __tree_is_empty(path: Path):
+    return len(__subfiles(path)) == 0 and all([__tree_is_empty(subfolder) for subfolder in __subfolders(path)])
