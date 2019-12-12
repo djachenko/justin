3a87630fe9103f8c68309696e16ad48a1fae4de6
Igor Djachenko <i.s.djachenko@gmail.com>
2019-12-13 05:30:23 +0700

    Reformatted IDE runner
    It should be easier to build commands

--- v3_0/runners/ide_runner.py
+++ v3_0/runners/ide_runner.py
@@ -3,18 +3,40 @@ from pathlib import Path
 from v3_0.runners import general_runner
 
 if __name__ == '__main__':
+    stage_commands = [
+        "develop",
+        "ourate",
+        "ready",
+        "publish",
+        "archive",
+        "move",
+    ]
+
+    locations = [
+        "D:/",
+        "H:/",
+        "/Volumes/pestilence/"
+    ]
+
+    stages = [
+        "stage0.gif",
+        "stage2.develop",
+        "stage2.ourate",
+        "stage4.published",
+    ]
+
+    def build_command(command, location, stage, name):
+        return f"{stage_commands[command]} {locations[location]}photos/stages/{stages[stage]}/{name}"
+
+
     commands = [
-        "schedule D:/photos/stages/stage2.develop/19.03.22.*",
-        "publish D:/photos/stages/stage0.gif/17.06.29.nsu_holi",
-        "ourate /Volumes/pestilence/photos/stages/stage2.ourate/19.11.17*",
+        build_command(2, 1, 2, "19.10.11**"),
         "upload -s 1",
         "local_sync",
         "rearrange -s 1",
-        "archive D:/photos/stages/stage4.published/18.04.25.*",
-        "move H:/photos/stages/stage2.develop/19.11.02.photonight",
     ]
 
     general_runner.run(
         Path(__file__).parent.parent.parent,
-        commands[2].split()
+        commands[0].split()
     )
