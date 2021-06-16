792f0418e7b808eb2920e4f7d264a62873e8d54d
Igor Djachenko <i.s.djachenko@gmail.com>
2021-06-17 03:55:02 +0700

    Removed unused feature

--- justin/shared/locations/locations.py
+++ justin/shared/locations/locations.py
@@ -5,32 +5,16 @@ from typing import List
 
 
 class Locations:
+    @abstractmethod
     def locations(self) -> List[Path]:
-        assert self.main() not in self.secondary_locations()
-
-        return [self.main()] + self.secondary_locations()
-
-    @abstractmethod
-    def main(self) -> Path:
-        pass
-
-    @abstractmethod
-    def secondary_locations(self) -> List[Path]:
         pass
 
 
 class WindowsLocations(Locations):
-    def main(self) -> Path:
-        return Path("D:/")
-
-    def secondary_locations(self) -> List[Path]:
-        return [Path.home()] + \
-               [Path(disk_letter + ":/") for disk_letter in string.ascii_uppercase if disk_letter != "D"]
+    def locations(self) -> List[Path]:
+        return [Path.home()] + [Path(disk_letter + ":/") for disk_letter in string.ascii_uppercase]
 
 
 class MacOSLocations(Locations):
-    def main(self) -> Path:
-        return Path.home()
-
-    def secondary_locations(self) -> List[Path]:
-        return list(Path("/Volumes").iterdir())
+    def locations(self) -> List[Path]:
+        return [Path.home()] + list(Path("/Volumes").iterdir())
