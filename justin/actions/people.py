7abd7fce4535f0811a7c1fb1c6f91cc545e3b7b8
Igor Djachenko <i.s.djachenko@gmail.com>
2023-01-12 02:49:40 +0700

    something

--- justin/actions/people.py
+++ justin/actions/people.py
@@ -2,7 +2,7 @@ from argparse import ArgumentParser, Namespace
 from pathlib import Path
 from typing import List
 
-from justin.actions.named.destinations_aware_action import DestinationsAwareAction
+from justin.actions.destinations_aware_action import DestinationsAwareAction
 from justin.actions.pattern_action import Extra
 from justin.shared.context import Context
 from justin.shared.filesystem import Folder
@@ -10,6 +10,7 @@ from justin.shared.models.person import PeopleRegistry, Person
 from justin.shared.models.photoset import Photoset
 from justin_utils import util
 from justin_utils.cli import Action, Parameter
+from pyvko.aspects.groups import Group
 from pyvko.entities.user import User
 from pyvko.pyvko_main import Pyvko
 
@@ -21,37 +22,45 @@ class FixPeopleMixin:
         folder = person.folder
 
         if not person.vk_id:
-            url = input(f"Who is {folder} in vk? ")
+            url = input(f"Who is {folder} in vk ('-' - abort, empty - empty)? ")
 
             if url == "-":
                 return
         else:
             url = input(f"Who is {folder} in vk? ") or person.vk_id
 
-        user: User = pyvko.get(url)
+        if url:
+            user: User = pyvko.get(url)
 
-        assert isinstance(user, User)
+            vk_id = user.id
 
-        vk_id = user.id
-        vk_name = f"{user.first_name} {user.last_name}"
+            if isinstance(user, User):
+                vk_name = f"{user.first_name} {user.last_name}"
+            elif isinstance(user, Group):
+                vk_name = user.name
+            else:
+                assert False
 
-        if not person.name:
-            new_name = input(f"Is it {vk_name}? (empty - yes, any string - new name) ") or vk_name
+            if not person.name:
+                new_name = input(f"Is it {vk_name}? (empty - yes, any string - new name) ") or vk_name
+            else:
+                other = "other"
+
+                new_name = util.ask_for_choice(f"Who is {person.folder}?", [
+                    person.name,
+                    vk_name,
+                    other
+                ])
+
+                if new_name == other:
+                    while True:
+                        new_name = input("Who then?")
+
+                        if new_name:
+                            break
         else:
-            other = "other"
-
-            new_name = util.ask_for_choice(f"Who is {person.folder}?", [
-                person.name,
-                vk_name,
-                other
-            ])
-
-            if new_name == other:
-                while True:
-                    new_name = input("Who then?")
-
-                    if new_name:
-                        break
+            vk_id = None
+            new_name = None
 
         person.vk_id = vk_id
         person.name = new_name
@@ -60,22 +69,42 @@ class FixPeopleMixin:
 
 
 class RegisterPeopleAction(DestinationsAwareAction, FixPeopleMixin):
+    __SET_NAME = "set_name"
+
+    def perform_for_photoset(self, photoset: Photoset, args: Namespace, context: Context, extra: Extra) -> None:
+        extra[RegisterPeopleAction.__SET_NAME] = photoset.name
+
+        super().perform_for_photoset(photoset, args, context, extra)
+
     def handle_closed(self, closed_folder: Folder, context: Context, extra: Extra) -> None:
         super().handle_closed(closed_folder, context, extra)
 
-        self.__register_from_path(closed_folder, context.pyvko, context.closed)
+        self.__register_from_path(closed_folder, context.pyvko, context.closed, extra[RegisterPeopleAction.__SET_NAME])
+
+    def handle_drive(self, drive_folder: Folder, context: Context, extra: Extra) -> None:
+        super().handle_drive(drive_folder, context, extra)
+
+        self.__register_from_path(drive_folder, context.pyvko, context.closed, extra[RegisterPeopleAction.__SET_NAME])
 
     def handle_my_people(self, my_people_folder: Folder, context: Context, extra: Extra) -> None:
         super().handle_my_people(my_people_folder, context, extra)
 
-        self.__register_from_path(my_people_folder, context.pyvko, context.my_people)
+        self.__register_from_path(my_people_folder, context.pyvko, context.my_people,
+                                  extra[RegisterPeopleAction.__SET_NAME])
 
     def handle_common(self, folder: Folder, context: Context, extra: Extra) -> None:
         pass
 
-    def __register_from_path(self, tree: Folder, pyvko: Pyvko, register: PeopleRegistry):
+    def __register_from_path(self, tree: Folder, pyvko: Pyvko, register: PeopleRegistry, source: str) -> None:
         for my_person in tree.subfolders:
-            if my_person.name in register:
+            if my_person.name in register or my_person.name.startswith("unknown"):
+                continue
+
+            collision = register.find_collision(my_person.name)
+
+            if collision:
+                print(f"{my_person.name} collides with {collision.folder}")
+
                 continue
 
             # noinspection PyTypeChecker
@@ -84,7 +113,7 @@ class RegisterPeopleAction(DestinationsAwareAction, FixPeopleMixin):
                     vk_id=None,
                     name=None,
                     folder=my_person.stem,
-                    source=tree.name,
+                    source=source,
                 ),
                 register,
                 pyvko
